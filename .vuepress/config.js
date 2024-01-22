160666d70f8dc8b93520dc3aa591585e8885e43e
KezhiAdore <1538001851@qq.com>
2024-01-22 17:02:39 +0800

    add ci

---
+++ .vuepress/config.js
@@ -0,0 +1,8 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  lang: 'zh-CN',
+  title: '你好， VuePress ！',
+  description: '这是我的第一个 VuePress 站点',
+  base: '/rldoc/'
+})
